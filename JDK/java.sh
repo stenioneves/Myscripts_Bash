df8f0d2605fa53d1db720e1496ae0400e0842a32
Stenio Neves <stenioneves@ymail.com>
2020-04-24 12:39:45 -0300

    Remoção da opção NPAPI
    A linha comentada habilitava o java no Navegador Mozilla onde já não há mais suporte

--- JDK/java.sh
+++ JDK/java.sh
@@ -7,5 +7,6 @@ echo 'Digite o nome da pasta'
 read PASTA
 mv $PASTA /usr/lib/jvm/
 update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/$PASTA/bin/java" 1
-update-alternatives --install "/usr/lib/mozilla/plugins/libjavaplugin.so" "mozilla-javaplugin.so" "/usr/lib/jvm/$PASTA/lib/i386/libnpjp2.so" 1
+#Essa opção foi descontinuada suporte NPAPI nos navegadores Mozilla e Chrome
+#update-alternatives --install "/usr/lib/mozilla/plugins/libjavaplugin.so" "mozilla-javaplugin.so" "/usr/lib/jvm/$PASTA/lib/i386/libnpjp2.so" 1
 update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/$PASTA/bin/javaws" 1
